d1f4dbcce97b8710d076bb64804dab0a45e145d5
Pedro Márcio <66282747+pedromos16@users.noreply.github.com>
2023-08-24 21:49:10 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,25 @@
+# Solicitar dois números ao usuário
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Solicitar a operação desejada ao usuário
+operacao = input("Escolha a operação (+, -, *, /, power): ")
+
+# Realizar a operação escolhida e exibir o resultado
+if operacao == "+":
+    resultado = num1 + num2
+elif operacao == "-":
+    resultado = num1 - num2
+elif operacao == "*":
+    resultado = num1 * num2
+elif operacao == "power":
+    resultado = num1*(num1 * num2)
+elif operacao == "/":
+    if num2 == 0:
+        resultado = "Erro: divisão por zero"
+    else:
+        resultado = num1 / num2
+else:
+    resultado = "Operação inválida"
+
+print(f"Resultado: {resultado}")
